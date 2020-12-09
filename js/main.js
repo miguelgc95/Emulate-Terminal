0a3144c9b620826327be903661ae083b904e4ec5
Cristina Bulnes <cristinabulnesmaya@gmail.com>
2020-12-09 17:36:43 +0100

    trial echo funcionality

--- js/main.js
+++ js/main.js
@@ -49,6 +49,69 @@ function mkdir() {
 function echo(commandline) {
     //let directoryTree = JSON.parse(localStorage.getItem("directoryTree"))
 
+    let directoryTree = [
+    /* 0 */ [
+                /* 0 -> folderObject === CARPETA1*/ {
+                    name: "carpeta1",
+                    folders: [  // CARPETAS DENTRO DE CARPETA1
+                /* 0.folders */        /* 0 -> folderObject === CARPETA1 DENTRO DE CARPETA1*/ {
+                                name: "carpeta1.1",
+                                folders: [],
+                                files: []
+                        },
+                /* 0.folders */       /* 1 -> folderObject === CARPETA2 DENTRO DE CARPETA1*/ {
+                                name: "carpeta1.2",
+                                folders: [],
+                                files: [
+                                        ]
+                        }
+                    ],
+                    files: [  // ARCHIVOS DENTRO DE CARPETA1
+                /* 0.files */        /* 0 -> fileObject === Archivo1 DENTRO DE CARPETA1*/ {
+                                        name: "Archivo1.1",
+                                        content: "contenido Archivo1.1"
+                                },
+                /* 0.files */       /* 1 -> folderObject === Archivo2 DENTRO DE CARPETA1*/ {
+                                        name: "Archivo1.2",
+                                        content: "contenido Archivo1.2"
+                                }
+                            ]
+                },
+                /* 1 -> folderObject === CARPETA2*/ {
+                    name: "carpeta2",
+                    folders: [  // CARPETAS DENTRO DE CARPETA2
+                /* 1.folders */        /* 0 -> folderObject === CARPETA1 DENTRO DE CARPETA2*/ {
+                                name: "carpeta2.1",
+                                folders: [],
+                                files: [
+                                        ]
+                        },
+                    ],
+                    files: [  // ARCHIVOS DENTRO DE CARPETA2
+                /* 1.files */        /* 0 -> fileObject === Archivo1 DENTRO DE CARPETA2*/ {
+                                        name: "Archivo1.2",
+                                        content: "contenido Archivo1.2"
+                                },
+                            ]
+                }
+        ],
+
+    /* 1 */ [
+        /* 0 -> fileObject === File1 del rootActual*/ {
+            name: "file1",
+            content: "contentFile1"
+            },
+        /* 1 -> fileObject === File2 del rootActual*/ {
+            name: "file2",
+            content: "contentFile2"
+            }
+        ]
+    ];
+    console.log(directoryTree);
+    //obtencion de la ruta llamando a la funcion:
+    // ejemplo: rutaPrueba = carpera1/carpera1.1
+    // situacion dentro del local Store: directoryTree[0].folders[0].files.push
+
     let echoParameters = [];
     let textToEvalue;
     for (let index = 2; index < commandline.length; index++) {
@@ -56,7 +119,8 @@ function echo(commandline) {
     }
     //console.log(echoParameters);
     textToEvalue = echoParameters.join("");
-    if (/^>>/.test(textToEvalue)) {
+    console.log(textToEvalue);
+    if (textToEvalue.includes(">>")) {
         echoParameters = textToEvalue.split(">>");
         var newfileObject = {
             name: echoParameters[1],
@@ -69,14 +133,19 @@ function echo(commandline) {
                 }
             });
 
-    } else if (/^>/.test(textToEvalue)) {
+    } else if (textToEvalue.includes(">")) {
         echoParameters = textToEvalue.split(">");
         var newfileObject = {
             name: echoParameters[1],
-            text: echoParameters[0],
+            content: echoParameters[0],
             }
 
-        actualDirectory.files.push(newfileObject);
+       // actualDirectory.files.push(newfileObject);
+        console.log(newfileObject);
+        console.log(directoryTree[0][0].folders)
+
+        directoryTree[0][0].folders[0].files.push(newfileObject);
+        console.log(directoryTree);
     } else {
         alert("no");
     }
